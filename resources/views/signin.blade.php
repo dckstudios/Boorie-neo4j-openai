b980b8364dc8306f9cd91219ce15cb3fcb68cf84
Rami Aloui <rami.aloui@novastudio.es>
2024-04-24 18:05:29 +0200

    aniadir template to boorie

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -10,7 +10,8 @@
     <div class="row justify-content-center">
       <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
         <div class="card border border-light-subtle rounded-3 shadow-sm">
-          <div class="card-body p-3 p-md-4 p-xl-5">
+          <div style="box-shadow:
+                10px 10px 22px rgba(0, 0, 0, 0.2);" class="card-body p-3 p-md-4 p-xl-5">
             <div class="text-center mb-3">
               <a href="#!">
                 <img src="/images/logo_boorie.png" alt="Boorie Logo" width="175" height="57">
