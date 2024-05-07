35133521aac8c7169488f813e67b38f143737784
Rami Aloui <rami.aloui@novastudio.es>
2024-05-07 14:01:29 +0200

    integration langchain boorie

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -8,6 +8,23 @@ class Usuario extends Model
 {
     protected $table = 'usuario';
     protected $fillable = ['username','usermail','password','updated_at','created_at','userimg'];
+     /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password'
+        
+    ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed'
+    ];
 
 }
